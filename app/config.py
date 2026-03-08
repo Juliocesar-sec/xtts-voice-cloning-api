57350882eef4826ea7d95a3d45b686f194b862af
Juliocesar-sec <jcmelgaco.cc@gmail.com>
2026-03-08 09:48:37 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
-import os
-
+# Pega a raiz do projeto independente de onde o script é chamado
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 OUTPUTS_DIR = os.path.join(BASE_DIR, "outputs")
 VOICES_DIR = os.path.join(BASE_DIR, "voices")
+MODELS_DIR = os.path.join(BASE_DIR, "models")
 
+# Garante a criação das pastas ao iniciar a API
 os.makedirs(OUTPUTS_DIR, exist_ok=True)
 os.makedirs(VOICES_DIR, exist_ok=True)
+os.makedirs(MODELS_DIR, exist_ok=True)
